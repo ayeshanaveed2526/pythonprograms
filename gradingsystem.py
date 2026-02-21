902f01c9dabc14c5c139dce2cfa6255f76c53c60
Ayesha Naveed <hr34772@gmail.com>
2026-02-21 05:53:37 +0500

    mini problem

---
+++ gradingsystem.py
@@ -0,0 +1,13 @@
+for i in range(1,6):
+    i = int(input("Enter your marks: "))
+    if i >= 90:
+        print("Grade: A")
+    elif i >= 80:
+        print("Grade: B")
+    elif i >= 70:
+        print("Grade: C")
+    elif i >= 60:
+        print("Grade: D")
+    else:
+        print("Grade: F")
+        
