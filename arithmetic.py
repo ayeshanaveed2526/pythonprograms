c9edf3172c74649d03ab4c0ca347aa282a343887
Ayesha Naveed <hr34772@gmail.com>
2026-02-23 09:36:41 +0500

    pushh

---
+++ arithmetic.py
@@ -0,0 +1,12 @@
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+add = x + y
+sub = x - y
+mul = x * y       
+div = x / y
+mod = x % y
+print("Addition:", add)
+print("Subtraction:", sub)  
+print("Multiplication:", mul)
+print("Division:", div)
+print("Modulus:", mod)
