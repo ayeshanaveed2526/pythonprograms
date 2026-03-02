a360009e3469f239e76033629855c6afb3cb330d
Ayesha Naveed <hr34772@gmail.com>
2026-03-02 11:12:28 +0500

    quiz.py

---
+++ quizgen.py
@@ -0,0 +1,29 @@
+# # 
+# Create a Python program using Object Oriented Programming (OOP) to simulate a simple ATM Machine.
+
+# 🔹 Step 1: Create a Class
+# Create a class named:
+
+# 🔹 Step 2: Add Constructor with Parameters
+# Add the following properties using constructor:
+# Account Holder Name
+# Account Number
+# Balance
+
+# 🔹 Step 3: Create Methods
+# Create the following methods:
+# check_balance()
+# Display current balance
+# deposit(amount)
+# Add amount to balance
+# Display updated balance
+# withdraw(amount)
+# Subtract amount from balance
+# Display remaining balance
+# exit()
+# Display thank you message
+# 🔹 Step 4: Create Object
+# Create an object of ATM class:
+
+# 🔹 Step 5: Call Methods
+# Call all the methods using the object to simulate ATM transactions.
