5f0428f54e4045625533a6ec61ca4a9359da4a85
Ayesha Naveed <hr34772@gmail.com>
2026-03-11 10:01:11 +0500

    logical

---
+++ looping.py
@@ -0,0 +1,14 @@
+l = ["ayesha", "jaggu", "sara", "ali", "hassan"]
+age = [20, 21, 22, 23, 24]
+for i in age:
+    for j in l:
+        print(i, j)
+
+
+
+# adj = ["red", "big", "tasty"]
+# fruits = ["apple", "banana", "cherry"]
+# for x in adj:
+#     for y in fruits:
+#         print(x, y)
+
