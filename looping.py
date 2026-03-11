4418a94c8680963793f07f9e4b83d88277a3adbd
Ayesha Naveed <hr34772@gmail.com>
2026-03-11 10:29:17 +0500

    dummy

--- looping.py
+++ looping.py
@@ -6,9 +6,9 @@ for i in age:
 
 
 
-# adj = ["red", "big", "tasty"]
-# fruits = ["apple", "banana", "cherry"]
-# for x in adj:
-#     for y in fruits:
-#         print(x, y)
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+for x in adj:
+    for y in fruits:
+        print(x, y)
 
