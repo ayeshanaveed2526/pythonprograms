a1fb53417c9d279acb210db9f7542a4c24e589fa
Ayesha Naveed <hr34772@gmail.com>
2026-03-11 10:14:34 +0500

    chawan

--- range.py
+++ range.py
@@ -1,5 +1,11 @@
 for x in range(6):
-    if x == 9:
+    if x == 3:
+     break     
+    print(x)
+else:
+    print("Finally finished!")
+for x in range(6):
+    if x == 13:
      break     
     print(x)
 else:
