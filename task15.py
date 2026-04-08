9ab3b8e99288e65d680fa99bf8f2ad2c3185b6c7
Ayesha Naveed <hr34772@gmail.com>
2026-04-08 09:47:34 +0500

    commits

---
+++ task15.py
@@ -0,0 +1,13 @@
+#loops in python
+#for loop
+s = ["ayesha", "hassan", "saba", "anwshah"]
+for i in s:
+    print(i)
+#Iterating over characters of strings
+    s = "Geeks"
+for i in s:
+    print(i)
+
+#Using range() with For Loop
+for i in range(5):
+    print(i)
